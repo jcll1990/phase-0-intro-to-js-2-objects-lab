85e083bf4c67695bab064e48287f5fb97021d13e
Juan Carlos Lopez Larco <Jclopezlarco@gmail.com>
2023-08-09 15:21:14 -0400

    complet lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newemployee = { ...employee };
+  
+    newemployee[key] = value;
+  
+    return newemployee;
+  }
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee, key) {
+    let clone = { ...employee };
+    delete clone[key];
+    return clone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
